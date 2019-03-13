b597ba74e36c7e4e48495312a6539836671a9fbb
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-13 04:55:38 +0300

    fix classpath for launch cmd

--- launcher.sh
+++ launcher.sh
@@ -5,16 +5,19 @@ COMPILE_OUT="compile-out"
 rm -rf $COMPILE_OUT
 mkdir $COMPILE_OUT
 
-for sourceSet in "src" "test"; do
+CLASSPATH="$(find lib -name \*.jar | tr "\n" ":")$COMPILE_OUT"
+
+for sourceSet in src test; do
   javac \
-    -classpath "$(find lib -name \*.jar | tr "\n" ":")$COMPILE_OUT" \
+    -classpath "$CLASSPATH" \
     -d $COMPILE_OUT \
     $(find $sourceSet -name \*.java)
 done
 
 java \
   -jar lib/junit-platform-console-standalone-1.4.0.jar \
-  --class-path $COMPILE_OUT \
+  --class-path "$CLASSPATH" \
+  --disable-banner \
   --include-engine=junit-jupiter \
   --exclude-engine=junit-vintage \
   @tests.txt
