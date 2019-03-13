7492b8c8c2e01636a017e891cc4092a9cafb82b6
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-13 17:25:07 +0300

    IfElse task

---
+++ src/by/stormnet/task/conditionals/IfElseTask.java
@@ -0,0 +1,38 @@
+package by.stormnet.task.conditionals;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class IfElseTask {
+
+  /**
+   * Используя блок if/else, напишите код, который вернет
+   * значение 42, если результат умножения x на y больше 10.
+   * Если результат умножения x на у меньше или равен 10,
+   * то следует вернуть разность между x и y.
+   */
+  int ifElse(int x, int y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя блок if/else if, напишите код, который вернет целую часть
+   * от результата деления x на y умноженную:
+   *   - на 2, если x меньше 10;
+   *   - на 3, если x больше либо равно 10 и x меньше 15;
+   *   - на 4, если x больше либо равно 15 и y больше 15.
+   */
+  int ifElseIf(float x, float y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя блок if/else if/else, напишите код, который вернет
+   * x умноженный:
+   *   - на 2, если x четное число;
+   *   - на 3, если x без остатка делится на 3.
+   * Если не выполняется ни одно из условий, то нужно вернуть исходное значение x.
+   */
+  int ifElseIfElse(int x) {
+    throw new TaskNotImplementedException();
+  }
+}
