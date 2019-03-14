56069ec046468497967fcc0e64317f8fa1314cb7
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-15 01:53:50 +0300

    launcher: print info about environment; add more verification tests for launcher

--- test/by/stormnet/VerifyLauncherTest.java
+++ test/by/stormnet/VerifyLauncherTest.java
@@ -3,17 +3,20 @@ package by.stormnet;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VerifyLauncherTest {
 
+  @Test
+  void verify() {
+    assertThat(1 + 1, equalTo(2));
+  }
+
   @ParameterizedTest
-  @CsvSource({
-      "1, 1",
-      "-1, -1"
-  })
-  void verify(int a, int b) {
-    assertThat(a, equalTo(b));
+  @ValueSource(ints = { 1 })
+  void parameterizedVerify(int value) {
+    assertThat(value, equalTo(1));
   }
 }
