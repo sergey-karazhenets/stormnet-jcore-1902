87a1dfb64d5547e9b7e03faa7a49f52ec7e70260
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-15 05:12:26 +0300

    task for 'switch' block

---
+++ src/by/stormnet/task/conditionals/SwitchTask.java
@@ -0,0 +1,28 @@
+package by.stormnet.task.conditionals;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class SwitchTask {
+
+  /**
+   * Напишите калькулятор, используя блок switch.
+   *
+   * x и y это значения, над которыми нужно будет делать
+   * операцию, заданную в переменной operator.
+   *
+   * operator может принимать одно из следующих значений:
+   *    "+", "-", "*", "/", "%", "range_sum".
+   * Если на вход пришел оператор, не описанный выше,
+   * то нужно вернуть результат умножения x на 10.
+   *
+   * Оператор "range_sum" обозначает то, что нужно вычислить
+   * сумму чисел от x до y (не включительно).
+   *
+   * К примеру,
+   *  -> operator = "-", тогда нужно посчитать разность между x и y
+   *  -> operator = "%", тогда нужно посчитать остаток от деления x на y
+   */
+  float calculator(int x, int y, String operator) {
+    throw new TaskNotImplementedException();
+  }
+}
