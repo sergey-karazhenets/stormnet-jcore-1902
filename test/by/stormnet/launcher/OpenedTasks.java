cf124c8aceb82178df10e00b792327e4e189a049
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-16 02:48:38 +0300

    move tasks to launch from text file to Java code

--- test/by/stormnet/launcher/OpenedTasks.java
+++ test/by/stormnet/launcher/OpenedTasks.java
@@ -1,10 +1,30 @@
 package by.stormnet.launcher;
 
-import java.util.Collections;
+import by.stormnet.task.arrays.ArraysTaskTest;
+import by.stormnet.task.conditionals.IfElseTaskTest;
+import by.stormnet.task.conditionals.SwitchTaskTest;
+import by.stormnet.task.loops.DoWhileLoopTaskTest;
+import by.stormnet.task.loops.ForLoopTaskTest;
+import by.stormnet.task.loops.WhileLoopTaskTest;
+import by.stormnet.task.numsys.NumeralSystemsTaskTest;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface OpenedTasks {
 
-  List<Class> classes = Collections.singletonList(VerifyLauncherTest.class);
-  List<String> packages = Collections.emptyList();
+  List<Class> classes = Stream.of(
+      NumeralSystemsTaskTest.class,
+      IfElseTaskTest.class,
+      SwitchTaskTest.class,
+      DoWhileLoopTaskTest.class,
+      ForLoopTaskTest.class,
+      WhileLoopTaskTest.class,
+      ArraysTaskTest.class
+  ).collect(Collectors.toList());
+
+  List<String> packages = Stream.of(
+      "by.stormnet.task.types",
+      "by.stormnet.task.operators"
+  ).collect(Collectors.toList());
 }
