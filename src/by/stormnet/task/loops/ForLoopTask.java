3c9371238690ead522a7ac925d66242aa148ada8
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-15 03:56:10 +0300

    add tasks for loops

---
+++ src/by/stormnet/task/loops/ForLoopTask.java
@@ -0,0 +1,63 @@
+package by.stormnet.task.loops;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class ForLoopTask {
+
+  /**
+   * Используя цикл for напишие код, который вернет
+   * сумму чисел от 1 до 57 (не включительно).
+   */
+  int calculateSum() {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл for напишие код, который вернет
+   * сумму чисел от x до y (не включительно).
+   * Учитывайте, что:
+   *  - если x равно y, то нужно вернуть 0
+   *  - если x меньше y, то значения x будут возрастать на каждой итерации
+   *  - если x больше y, то значения x будут убывать на каждой итерации
+   */
+  int calculateSum(int x, int y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл for напишие код, который вернет
+   * разность чисел от x до y (включительно).
+   * Учитывайте, что:
+   *  - если x меньше y, то значения x будут возрастать на каждой итерации
+   *  - если x больше y, то значения x будут убывать на каждой итерации
+   */
+  int calculateSub(int x, int y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл for напишие код, который вернет
+   * произведение только четных чисел от 1 до 42 (не включительно).
+   */
+  long calculateEvenMultiply() {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл for напишие код, который будет считать
+   * всю таблицу умножения и суммировать результаты умножения.
+   *
+   * К примеру, считаем таблицу умножения:
+   *  1 * 1 = 0
+   *  1 * 2 = 2
+   *  1 * 9 = 9
+   *  ...
+   *  9 * 1 = 9
+   *  9 * 2 = 18
+   *
+   * Суммируем результаты: 0 + 2 + 9 + ... + 9 + 18
+   */
+  int sumOfMultiplicationTable() {
+    throw new TaskNotImplementedException();
+  }
+}
---
+++ test/by/stormnet/task/loops/WhileLoopTaskTest.java
@@ -0,0 +1,53 @@
+package by.stormnet.task.loops;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class WhileLoopTaskTest {
+
+  private final WhileLoopTask task = new WhileLoopTask();
+
+  @Test
+  void calculateSum() {
+    assertThat(task.calculateSum(), equalTo(1596));
+  }
+
+  @ParameterizedTest(name = "[{index}] x: {0}, y: {1}, expected: {2}")
+  @CsvSource({
+      "0, 0, 0",
+      "0, 1, 0",
+      "1, 1, 0",
+      "1, 2, 1",
+      "1, 3, 3",
+      "1, 4, 6",
+      "1, 57, 1596",
+      "-10, 6, -40",
+      "5, 0, 15",
+  })
+  void calculateSum(int x, int y, int expected) {
+    assertThat(task.calculateSum(x, y), equalTo(expected));
+  }
+
+  @ParameterizedTest(name = "[{index}] x: {0}, y: {1}, expected: {2}")
+  @CsvSource({
+      "0, 0, 0",
+      "1, 1, 0",
+      "0, 1, -1",
+      "1, 1, 0",
+      "1, 2, -1",
+      "5, 1, -5",
+      "-5, 1, 4"
+  })
+  void calculateSub(int x, int y, int expected) {
+    assertThat(task.calculateSub(x, y), equalTo(expected));
+  }
+
+  @Test
+  void calculateEvenMultiply() {
+    assertThat(task.calculateEvenMultiply(), equalTo(8631196239733456896L));
+  }
+}
