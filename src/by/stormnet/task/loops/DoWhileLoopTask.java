15cf117f45a9a273bfcd97225c2fa0c269e55b06
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-15 03:56:10 +0300

    add tasks for loops

---
+++ src/by/stormnet/task/loops/DoWhileLoopTask.java
@@ -0,0 +1,45 @@
+package by.stormnet.task.loops;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class DoWhileLoopTask {
+
+  /**
+   * Используя цикл do-while напишие код, который вернет
+   * сумму чисел от 1 до 57 (не включительно).
+   */
+  int calculateSum() {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл do-while напишие код, который вернет
+   * сумму чисел от x до y (не включительно).
+   * Учитывайте, что:
+   *  - если x равно y, то нужно вернуть 0
+   *  - если x меньше y, то значения x будут возрастать на каждой итерации
+   *  - если x больше y, то значения x будут убывать на каждой итерации
+   */
+  int calculateSum(int x, int y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл do-while напишие код, который вернет
+   * разность чисел от x до y (включительно).
+   * Учитывайте, что:
+   *  - если x меньше y, то значения x будут возрастать на каждой итерации
+   *  - если x больше y, то значения x будут убывать на каждой итерации
+   */
+  int calculateSub(int x, int y) {
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Используя цикл do-while напишие код, который вернет
+   * произведение только четных чисел от 1 до 42 (не включительно).
+   */
+  long calculateEvenMultiply() {
+    throw new TaskNotImplementedException();
+  }
+}
