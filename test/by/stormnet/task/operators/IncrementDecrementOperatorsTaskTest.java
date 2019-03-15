8154f012fe606147f5a6a497b1d7578797804d2f
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-16 02:29:27 +0300

    launch opened tasks from Java

--- test/by/stormnet/task/operators/IncrementDecrementOperatorsTaskTest.java
+++ test/by/stormnet/task/operators/IncrementDecrementOperatorsTaskTest.java
@@ -6,7 +6,7 @@ import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class IncrementDecrementOperatorsTaskTest {
+public class IncrementDecrementOperatorsTaskTest {
 
   private final IncrementDecrementOperatorsTask task = new IncrementDecrementOperatorsTask();
 
