e0610e69dede2515f3e0d7d2b30cb1a3d290f7ed
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-16 02:29:27 +0300

    launch opened tasks from Java

--- test/by/stormnet/task/loops/DoWhileLoopTaskTest.java
+++ test/by/stormnet/task/loops/DoWhileLoopTaskTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class DoWhileLoopTaskTest {
+public class DoWhileLoopTaskTest {
 
   private final DoWhileLoopTask task = new DoWhileLoopTask();
 
--- test/by/stormnet/task/types/DoubleBasicsTaskTest.java
+++ test/by/stormnet/task/types/DoubleBasicsTaskTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.jupiter.api.Test;
 
-class DoubleBasicsTaskTest {
+public class DoubleBasicsTaskTest {
 
   private final DoubleBasicsTask task = new DoubleBasicsTask();
 
