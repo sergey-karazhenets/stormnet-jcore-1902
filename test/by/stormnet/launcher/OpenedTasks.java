8006b08cff4a85668c4426bae9764020a54a62a2
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-16 02:29:27 +0300

    launch opened tasks from Java

--- test/by/stormnet/launcher/OpenedTasks.java
+++ test/by/stormnet/launcher/OpenedTasks.java
@@ -1,26 +1,10 @@
 package by.stormnet.launcher;
 
-import by.stormnet.task.arrays.ArraysTaskTest;
-import by.stormnet.task.conditionals.IfElseTaskTest;
-import by.stormnet.task.conditionals.SwitchTaskTest;
-import by.stormnet.task.loops.DoWhileLoopTaskTest;
-import by.stormnet.task.loops.ForLoopTaskTest;
-import by.stormnet.task.loops.WhileLoopTaskTest;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public interface OpenedTasks {
 
-  List<Class> classes = Stream.of(
-      IfElseTaskTest.class,
-      SwitchTaskTest.class,
-      DoWhileLoopTaskTest.class,
-      ForLoopTaskTest.class,
-      WhileLoopTaskTest.class,
-      ArraysTaskTest.class
-  ).collect(Collectors.toList());
-  
+  List<Class> classes = Collections.singletonList(VerifyLauncherTest.class);
   List<String> packages = Collections.emptyList();
 }
