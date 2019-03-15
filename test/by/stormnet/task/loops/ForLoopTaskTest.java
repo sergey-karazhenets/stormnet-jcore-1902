b3dbf8931b8411dd0861dd58cc1cae0e7aafadb1
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-16 02:29:27 +0300

    launch opened tasks from Java

--- test/by/stormnet/task/loops/ForLoopTaskTest.java
+++ test/by/stormnet/task/loops/ForLoopTaskTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class ForLoopTaskTest {
+public class ForLoopTaskTest {
 
   private final ForLoopTask task = new ForLoopTask();
 
