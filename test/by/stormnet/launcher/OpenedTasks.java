05bc83ad52a3fd31d7248cf66688085c1ee5d2ae
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-03-21 01:54:17 +0300

    add task to sort an array and task to play with on zero dividing

--- test/by/stormnet/launcher/OpenedTasks.java
+++ test/by/stormnet/launcher/OpenedTasks.java
@@ -1,10 +1,30 @@
 package by.stormnet.launcher;
 
+import by.stormnet.task.arrays.ArraySortTaskTest;
+import by.stormnet.task.arrays.ArraysTaskTest;
+import by.stormnet.task.conditionals.IfElseTaskTest;
+import by.stormnet.task.conditionals.SwitchTaskTest;
+import by.stormnet.task.loops.DoWhileLoopTaskTest;
+import by.stormnet.task.loops.ForLoopTaskTest;
+import by.stormnet.task.loops.WhileLoopTaskTest;
+import by.stormnet.task.math.DivideOnZeroTaskTest;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface OpenedTasks {
 
-  List<Class> classes = Collections.singletonList(VerifyLauncherTest.class);
+  List<Class> classes = Stream.of(
+      IfElseTaskTest.class,
+      SwitchTaskTest.class,
+      DoWhileLoopTaskTest.class,
+      ForLoopTaskTest.class,
+      WhileLoopTaskTest.class,
+      ArraysTaskTest.class,
+      ArraySortTaskTest.class,
+      DivideOnZeroTaskTest.class
+  ).collect(Collectors.toList());
+  
   List<String> packages = Collections.emptyList();
 }
