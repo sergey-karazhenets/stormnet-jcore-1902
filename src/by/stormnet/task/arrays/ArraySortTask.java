96223e8afb234541314f656b5b8fe9122fbed143
MichaelBeliaev <michaelbeliaev123@gmail.com>
2019-03-22 19:53:25 +0300

    BUBBLE

--- src/by/stormnet/task/arrays/ArraySortTask.java
+++ src/by/stormnet/task/arrays/ArraySortTask.java
@@ -9,14 +9,30 @@ class ArraySortTask {
    * используя алгоритм сортировки пузырьком.
    */
   int[] bubbleSortAsc(int[] values) {
-    throw new TaskNotImplementedException();
-  }
+    int temp;
+    for (int i=0;i<values.length-1;) {
+      if (values[i]>values[i+1]) {
+        temp=values[i];
+        values[i]=values[i+1];
+        values[i+1]=temp;
+        i=0;} else i++;
+      }
+      return values;
+ }
 
   /**
    * Отсортировать числа в массиве в порядке их убывания,
    * используя алгоритм сортировки пузырьком.
    */
   int[] bubbleSortDesc(int[] values) {
-    throw new TaskNotImplementedException();
+    int temp;
+    for (int i=0;i<values.length-1;) {
+      if (values[i]<values[i+1]) {
+        temp=values[i];
+        values[i]=values[i+1];
+        values[i+1]=temp;
+        i=0;} else i++;
+    }
+    return values;
   }
 }
