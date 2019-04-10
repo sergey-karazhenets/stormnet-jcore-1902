39b77c40d95d7af828d1f1d42be5acbbda21fecc
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-04-10 18:28:59 +0300

    tasks for recursion and exceptions

---
+++ src/by/stormnet/task/recursion/RecursionTask.java
@@ -0,0 +1,21 @@
+package by.stormnet.task.recursion;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class RecursionTask {
+
+  /**
+   * Используя рекурсию, сделайте вычисление значения x!.
+   * x! это факториал числа x.
+   * Формула для расчета: x! = 1 * 2 * ... * x-1 * x.
+   *
+   * Например:
+   *  - 1! = 1
+   *  - 2! = 1 * 2 = 2
+   *  - 3! = 1 * 2 * 3 = 6
+   *  - 5! = 1 * 2 * 3 * 4 * 5 = 120
+   */
+  long factorial(int x) {
+    throw new TaskNotImplementedException();
+  }
+}
