0b12e6ba63e5a92e3055cd44aae517823de0fc21
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-04-11 04:34:42 +0300

    task for 'methods call'

---
+++ src/by/stormnet/task/methods/MethodsCallTaskLogic.java
@@ -0,0 +1,14 @@
+package by.stormnet.task.methods;
+
+import by.stormnet.task.TaskNotImplementedException;
+
+class MethodsCallTaskLogic {
+
+  void doNothing() {
+    // do nothing
+  }
+
+  String concatStrings(String str1, String str2) {
+    throw new TaskNotImplementedException();
+  }
+}
