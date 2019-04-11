c032c295d30d8082e563b72260b948020ecc8072
Sergey Karazhenets <sergeykarazhenets@gmail.com>
2019-04-11 03:24:00 +0300

    'exceptions' task

--- src/by/stormnet/task/exceptions/ExceptionsTask.java
+++ src/by/stormnet/task/exceptions/ExceptionsTask.java
@@ -1,59 +1,94 @@
 package by.stormnet.task.exceptions;
 
-import java.util.function.Supplier;
+import by.stormnet.task.TaskNotImplementedException;
 
 class ExceptionsTask {
 
   /**
-   * В этой строке {@code return logic.get();} может возникнуть исключение IllegalStateException.
-   * Сделайте обработку этого исключения таким образом, что:
-   *  - если исключение не возникает, то возвращается результат вызова {@code logic.get()}.
-   *  - если исключение возникает, то нужно возвратить число 0.
+   * На вход в метод приходит объект ExceptionsTaskLogic.
+   * У него есть методы calculateIntValue и recover1.
+   *
+   * Сделайте вызов метода calculateIntValue и верните полученное значение в качестве ответа на задание.
+   *
+   * При этом учтите, что метод calculateIntValue может выбрасывать исключение IllegalStateException.
+   *
+   * Поэтому дополнительно нужно сделать обработку исключения, и в случае, если оно возникает, то
+   * следует вызвать метод recover1 и результат вернуть в качестве ответа на задание.
    */
-  int handleException(Supplier<Integer> logic) {
-    return logic.get();
+  int handleException(ExceptionsTaskLogic logic) {
+    throw new TaskNotImplementedException();
   }
 
   /**
-   * В этой строке {@code return logic.get();} может возникнуть
-   * два исключения: IllegalStateException или IllegalArgumentException.
-   * Сделайте обработку этих исключений таким образом, что:
-   *  - если не возникает ни одного исключения, то возвращается результат вызова {@code logic.get()}.
-   *  - если возникает IllegalStateException, то нужно возвратить число 0.
-   *  - если возникает IllegalArgumentException, то нужно возвратить число 1.
+   * На вход в метод приходит объект ExceptionsTaskLogic.
+   * У него есть методы calculateIntValue, recover1 и recover2.
+   *
+   * Сделайте вызов метода calculateIntValue и верните полученное значение в качестве ответа на задание.
+   *
+   * При этом учтите, что метод calculateIntValue может выбрасывать одно из исключений:
+   * IllegalStateException или IllegalArgumentException.
+   *
+   * Поэтому дополнительно нужно сделать обработку обоих исключений.
+   * В случае, если возникает исключение:
+   *  - IllegalStateException, то следует вызвать метод recover1 и результат вернуть в качестве ответа на задание.
+   *  - IllegalArgumentException, то следует вызвать метод recover2 и результат вернуть в качестве ответа на задание.
    */
-  int handleManyExceptions(Supplier<Integer> logic) {
-    return logic.get();
+  int handleManyExceptions(ExceptionsTaskLogic logic) {
+    throw new TaskNotImplementedException();
   }
 
   /**
-   * В этой строке {@code return logic.get();} может возникнуть
-   * два исключения: IllegalStateException или IllegalArgumentException.
-   * Сделайте обработку этих исключений таким образом, что:
-   *  - если не возникает ни одного исключения, то возвращается результат вызова {@code logic.get()}.
-   *  - если возникает IllegalStateException или IllegalArgumentException, то нужно возвратить число 0.
+   * На вход в метод приходит объект ExceptionsTaskLogic.
+   * У него есть методы calculateIntValue, recover1 и recover2.
+   *
+   * Сделайте вызов метода calculateIntValue и верните полученное значение в качестве ответа на задание.
+   *
+   * При этом учтите, что метод calculateIntValue может выбрасывать одно из исключений:
+   * IllegalStateException или IllegalArgumentException.
+   *
+   * Поэтому дополнительно нужно сделать обработку обоих исключений.
+   * При возникновении любого из исключений в качестве ответа на задание
+   * нужно вернуть сумму результатов вызова методов recover1 и recover2.
+   *
+   * Для обработки обоих исключений используйте только один блок catch.
    */
-  int handleManyExceptions1(Supplier<Integer> logic) {
-    return logic.get();
+  int handleManyExceptions1(ExceptionsTaskLogic logic) {
+    throw new TaskNotImplementedException();
   }
 
   /**
-   * В этой строке {@code return logic.get();} может возникнуть исключение IllegalStateException.
-   * Используя блок try/catch/finally, сделайте обработку этого исключения таким образом, что:
-   *  - если исключение не возникает, то возвращается результат вызова {@code logic.get()}.
-   *  - если исключение возникает, то нужно возвратить число 0.
-   *  - в независимости от того возникло исключение или нет должен быть вызван метод {@code finallyLogic.run()}.
+   * На вход в метод приходит объект ExceptionsTaskLogic.
+   * У него есть методы calculateIntValue, recover1 и doFinally.
+   *
+   * Сделайте вызов метода calculateIntValue и верните полученное значение в качестве ответа на задание.
+   *
+   * При этом учтите, что метод calculateIntValue может выбрасывать исключение IllegalStateException.
+   *
+   * Поэтому дополнительно нужно сделать обработку исключения, и в случае, если оно возникает, то
+   * следует вызвать метод recover1 и результат вернуть в качестве ответа на задание.
+   *
+   * Также в независимости от того возникло исключение или нет нужно делать вызов метода doFinally.
    */
-  int tryCatchFinally(Supplier<Integer> logic, Runnable finallyLogic) {
-    return logic.get();
+  int handleExceptionWithFinally(ExceptionsTaskLogic logic) {
+    throw new TaskNotImplementedException();
   }
 
   /**
    * Напишите код так что:
-   *  - если x больше нуля, то должно быть сгенерировано исключение IllegalArgumentException.
-   *  - если x меньше либо равно нулю, то следует вернуть значение x.
+   *  - если x больше нуля, то должно быть сгенерировано исключение IllegalArgumentException,
+   *  - иначе следует вернуть значение x в качестве ответа на задание.
    */
   int throwException(int x) {
-    return 0;
+    throw new TaskNotImplementedException();
+  }
+
+  /**
+   * Напишите код так что:
+   *  - если x больше нуля, то должно быть сгенерировано исключение IllegalArgumentException
+   *    с сообщением об ошибке "Invalid value",
+   *  - иначе следует вернуть значение x в качестве ответа на задание.
+   */
+  int throwExceptionWithMessage(int x) {
+    throw new TaskNotImplementedException();
   }
 }
--- test/by/stormnet/task/exceptions/ExceptionsTaskTest.java
+++ test/by/stormnet/task/exceptions/ExceptionsTaskTest.java
@@ -3,89 +3,240 @@ package by.stormnet.task.exceptions;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.function.Supplier;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ExceptionsTaskTest {
 
   private final ExceptionsTask task = new ExceptionsTask();
 
-  @ParameterizedTest(name = "[{index}] expected: {1}")
+  @ParameterizedTest
   @MethodSource(value = "handleExceptionArgs")
-  void handleException(Supplier<Integer> logic, int expected) {
+  void handleException(ExceptionsTaskLogic logic, int expected) {
     assertThat(task.handleException(logic), equalTo(expected));
   }
 
   static Collection<Arguments> handleExceptionArgs() {
     return Arrays.asList(
-        Arguments.of((Supplier<Integer>) () -> 0, 0),
-        Arguments.of((Supplier<Integer>) () -> 1, 1),
-        Arguments.of((Supplier<Integer>) () -> 125, 125),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalStateException(); }, 0)
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 0,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            0
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 1,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            1
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 125,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            125
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalStateException(); },
+                () -> 323,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            323
+        )
     );
   }
 
-  @ParameterizedTest(name = "[{index}] expected: {1}")
+  @ParameterizedTest
   @MethodSource(value = "handleManyExceptionsArgs")
-  void handleManyExceptions(Supplier<Integer> logic, int expected) {
+  void handleManyExceptions(ExceptionsTaskLogic logic, int expected) {
     assertThat(task.handleManyExceptions(logic), equalTo(expected));
   }
 
   static Collection<Arguments> handleManyExceptionsArgs() {
     return Arrays.asList(
-        Arguments.of((Supplier<Integer>) () -> 0, 0),
-        Arguments.of((Supplier<Integer>) () -> 1, 1),
-        Arguments.of((Supplier<Integer>) () -> 125, 125),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalStateException(); }, 0),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalArgumentException(); }, 1)
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 0,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            0
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 1,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            1
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 125,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            125
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalStateException(); },
+                () -> 323,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            323
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalArgumentException(); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> 425
+            ),
+            425
+        )
     );
   }
 
-  @ParameterizedTest(name = "[{index}] expected: {1}")
+  @ParameterizedTest
   @MethodSource(value = "handleManyExceptions1Args")
-  void handleManyExceptions1(Supplier<Integer> logic, int expected) {
+  void handleManyExceptions1(ExceptionsTaskLogic logic, int expected) {
     assertThat(task.handleManyExceptions1(logic), equalTo(expected));
   }
 
   static Collection<Arguments> handleManyExceptions1Args() {
     return Arrays.asList(
-        Arguments.of((Supplier<Integer>) () -> 0, 0),
-        Arguments.of((Supplier<Integer>) () -> 1, 1),
-        Arguments.of((Supplier<Integer>) () -> 125, 125),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalStateException(); }, 0),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalArgumentException(); }, 0)
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 0,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            0
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 1,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            1
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 125,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            125
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalStateException(); },
+                () -> 32,
+                () -> 34
+            ),
+            66
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalArgumentException(); },
+                () -> 32,
+                () -> 45
+            ),
+            77
+        )
     );
   }
 
-  @ParameterizedTest(name = "[{index}] expected: {1}")
-  @MethodSource(value = "tryCatchFinallyArgs")
-  void tryCatchFinally(Supplier<Integer> logic, int expected) {
-    final Runnable finallyLogic = mock(Runnable.class);
-    assertThat(task.tryCatchFinally(logic, finallyLogic), equalTo(expected));
-    verify(finallyLogic, times(1)).run();
+  @ParameterizedTest
+  @MethodSource(value = "handleExceptionWithFinallyArgs")
+  void handleExceptionWithFinally(ExceptionsTaskLogic logic, int expected) {
+    final ExceptionsTaskLogic logicSpy = spy(logic);
+    assertThat(task.handleExceptionWithFinally(logicSpy), equalTo(expected));
+    verify(logicSpy, times(1)).doFinally();
   }
 
-  static Collection<Arguments> tryCatchFinallyArgs() {
+  static Collection<Arguments> handleExceptionWithFinallyArgs() {
     return Arrays.asList(
-        Arguments.of((Supplier<Integer>) () -> 0, 0),
-        Arguments.of((Supplier<Integer>) () -> 1, 1),
-        Arguments.of((Supplier<Integer>) () -> 125, 125),
-        Arguments.of((Supplier<Integer>) () -> { throw new IllegalStateException(); }, 0)
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 0,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            0
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 1,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            1
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> 125,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); },
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            125
+        ),
+        Arguments.of(
+            new ExceptionsTaskLogic(
+                () -> { throw new IllegalStateException(); },
+                () -> 323,
+                () -> { throw new IllegalStateException("Method shouldn't be called"); }
+            ),
+            323
+        )
     );
   }
 
-  @Test
-  void throwException() {
-    assertThat(task.throwException(0), equalTo(0));
-    assertThrows(IllegalArgumentException.class, () -> task.throwException(1));
+  @ParameterizedTest(name = "[{index}] x: {0}, expected: {1}")
+  @CsvSource({
+      "0, 0",
+      "-1, -1",
+      "1, exception"
+  })
+  void throwException(int x, String expected) {
+    if (x > 0) {
+      assertThrows(IllegalArgumentException.class, () -> task.throwException(x));
+    } else {
+      assertThat(task.throwException(x), equalTo(Integer.valueOf(expected)));
+    }
+  }
+
+  @ParameterizedTest(name = "[{index}] x: {0}, expected: {1}")
+  @CsvSource({
+      "0, 0",
+      "-1, -1",
+      "1, exception"
+  })
+  void throwExceptionWithMessage(int x, String expected) {
+    if (x > 0) {
+      final IllegalArgumentException thrown = assertThrows(
+          IllegalArgumentException.class,
+          () -> task.throwExceptionWithMessage(x)
+      );
+      assertThat(thrown.getMessage(), equalTo("Invalid value"));
+    } else {
+      assertThat(task.throwExceptionWithMessage(x), equalTo(Integer.valueOf(expected)));
+    }
   }
 }
