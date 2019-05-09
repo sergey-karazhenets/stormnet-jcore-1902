a826f947230f9274e8a02d814c927d322b9718b7
MarySaganovich <Smaile978@gmail.com>
2019-05-09 20:37:01 +0300

    ListTask

--- src/by/stormnet/task/collections/ListTask.java
+++ src/by/stormnet/task/collections/ListTask.java
@@ -1,7 +1,10 @@
 package by.stormnet.task.collections;
 
 import by.stormnet.task.TaskNotImplementedException;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class ListTask {
 
@@ -9,97 +12,116 @@ class ListTask {
    * Создать пустой ArrayList, в котором можно хранить int значения.
    */
   List<Integer> emptyIntArrayList() {
-    throw new TaskNotImplementedException();
+    List<Integer> emptyIntArrayList = new ArrayList<>();
+    return emptyIntArrayList;
   }
 
   /**
    * Создать пустой ArrayList, в котором можно хранить String значения.
    */
   List<String> emptyStringArrayList() {
-    throw new TaskNotImplementedException();
+    List<String> emptyStringArrayList = new ArrayList<>();
+    return emptyStringArrayList;
   }
 
   /**
    * Определить размер списка.
    */
   int listSize(List<Integer> values) {
-    throw new TaskNotImplementedException();
+    return values.size();
   }
 
   /**
    * Создайть ArrayList и заполнить его значениями из массива values.
    */
   List<Integer> intList(int... values) {
-    throw new TaskNotImplementedException();
+    List<Integer> il = new ArrayList<>();
+    for (Integer value:values) {
+      il.add(value);
+    }
+    return il;
   }
 
   /**
    * Получить элемент c индексом index из списка строк.
    */
   String elementAtIndex(List<String> strings, int index) {
-    throw new TaskNotImplementedException();
+    return strings.get(index);
   }
 
   /**
    * Добавить значение value в позицию index в списке values.
    */
   void addAtIndex(List<Long> values, int index, long value) {
-    throw new TaskNotImplementedException();
+    values.add(index, value);
+    return;
   }
 
   /**
    * Заменить в списке values значение в позиции index новым значением value.
    */
   void updateAtIndex(List<Long> values, int index, long value) {
-    throw new TaskNotImplementedException();
+    values.set(index, value);
+    return;
   }
 
   /**
    * Удалить из списка values значение в позиции index.
    */
   void removeAtIndex(List<Long> values, int index) {
-    throw new TaskNotImplementedException();
+    values.remove(index);
   }
 
   /**
    * Удалить из списка values значение value.
    */
   void removeValue(List<Long> values, long value) {
-    throw new TaskNotImplementedException();
+    values.remove(value);
   }
 
   /**
    * Проверить есть ли в списке строк strings строка str.
    */
   boolean hasElement(List<String> strings, String str) {
-    throw new TaskNotImplementedException();
+    if (strings.contains(str)) {
+      return true;
+    } else return false;
   }
 
   /**
    * Получить индекс элемента str в списке строк strings.
    */
   int indexOf(List<String> strings, String str) {
-    throw new TaskNotImplementedException();
+    return strings.indexOf(str);
   }
 
   /**
    * Удалить из списка все отрицательные значения и все элементы равные null.
    */
   void removeAllNullOrNegativeValues(List<Integer> values) {
-    throw new TaskNotImplementedException();
+      Iterator<Integer> itr = values.iterator();
+      while (itr.hasNext()) {
+        if (itr.next() == null) {
+          itr.remove();
+        }
+      }
+    values.removeIf(i-> (i < 0 ));
   }
 
   /**
    * Объединить три списка values1, values2 и values3 в один список.
    */
   List<Integer> mergeLists(List<Integer> values1, List<Integer> values2, List<Integer> values3) {
-    throw new TaskNotImplementedException();
+    List<Integer> newList = Stream.of(values1, values2, values3).flatMap(Collection::stream).collect(Collectors.toList());
+    return newList;
   }
 
   /**
    * Определить пустой ли список.
    */
   boolean isEmpty(List<String> strings) {
-    throw new TaskNotImplementedException();
+    if (strings.isEmpty()) {
+      return true;
+    } else return false;
   }
 }
