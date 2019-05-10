91751ed3b3e11d329331da3153ea2eac6bcc48b8
JanaKoncewaja <konzewajaaaa@gmail.com>
2019-05-10 23:56:37 +0300

    Готово

--- src/by/stormnet/task/collections/SetTask.java
+++ src/by/stormnet/task/collections/SetTask.java
@@ -1,6 +1,9 @@
 package by.stormnet.task.collections;
 
 import by.stormnet.task.TaskNotImplementedException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -10,76 +13,101 @@ class SetTask {
    * Создать пустой HashSet, в котором можно хранить int значения.
    */
   Set<Integer> emptyIntHashSet() {
-    throw new TaskNotImplementedException();
+
+    Set<Integer> set = new HashSet<>();
+    return set;
   }
 
   /**
    * Создать пустой HashSet, в котором можно хранить String значения.
    */
   Set<String> emptyStringHashSet() {
-    throw new TaskNotImplementedException();
+   Set<String> set = new HashSet<>();
+   return set;
   }
 
   /**
    * Определить размер множества.
    */
   int size(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+
+    int n =values.size();
+    return n;
   }
 
   /**
    * Создайть HashSet и заполнить его значениями из массива values.
    */
   Set<Integer> intSetFromArray(int... values) {
-    throw new TaskNotImplementedException();
+    Set<Integer> set1 = new HashSet<>();
+    for(int a: values){
+      set1.add(a);
+
+    }
+    return set1;
+
   }
 
   /**
    * Преобразовать список values в HashSet.
    */
   Set<Integer> intSetFromArrayList(List<Integer> values) {
-    throw new TaskNotImplementedException();
+
+    Set<Integer> set = new HashSet<>(values);
+    return set;
   }
 
   /**
    * Преобразовать множество values в ArrayList.
    */
   List<Integer> intSetToArrayList(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+
+    List<Integer> list = new ArrayList<>(values);
+    return list;
   }
 
   /**
    * Удалить из множества values значение value.
    */
   void removeValue(Set<Integer> values, int value) {
-    throw new TaskNotImplementedException();
+
+    values.remove(value);
   }
 
   /**
    * Проверить есть ли во множестве values значение value.
    */
   boolean hasElement(Set<Integer> values, int value) {
-    throw new TaskNotImplementedException();
+
+    boolean b =values.contains(value);
+    return b;
   }
 
   /**
    * Объединить три множества set1, set2, set3 в одно множество.
    */
   Set<Integer> mergeSets(Set<Integer> set1, Set<Integer> set2, Set<Integer> set3) {
-    throw new TaskNotImplementedException();
+  Set<Integer> n = new HashSet<>();
+  n.addAll(set1);
+  n.addAll(set2);
+  n.addAll(set3);
+  return n;
   }
 
   /**
    * Определить пустое ли множество.
    */
   boolean isEmpty(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+
+    boolean b = values.isEmpty();
+    return b;
   }
 
   /**
    * Проверить равны ли два множества между собой.
    */
   boolean isEquals(Set<Integer> set1, Set<Integer> set2) {
-    throw new TaskNotImplementedException();
+    boolean n = set1.equals(set2);
+    return n;
   }
 }
