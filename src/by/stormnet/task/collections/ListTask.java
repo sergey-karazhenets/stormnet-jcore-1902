04750b0401a51c81e914ca2e81329f88e52ef67f
DmitryZmitrovich <Dmitry230890@mail.ru>
2019-05-19 23:56:50 +0300

    Collections

--- src/by/stormnet/task/collections/ListTask.java
+++ src/by/stormnet/task/collections/ListTask.java
@@ -1,6 +1,8 @@
 package by.stormnet.task.collections;
 
 import by.stormnet.task.TaskNotImplementedException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class ListTask {
@@ -9,97 +11,119 @@ class ListTask {
    * Создать пустой ArrayList, в котором можно хранить int значения.
    */
   List<Integer> emptyIntArrayList() {
-    throw new TaskNotImplementedException();
+   List<Integer>ArrayList= new ArrayList<>();
+   return ArrayList;
   }
 
   /**
    * Создать пустой ArrayList, в котором можно хранить String значения.
    */
   List<String> emptyStringArrayList() {
-    throw new TaskNotImplementedException();
+    List<String>ArrayList= new ArrayList<>();
+    return ArrayList;
   }
 
   /**
    * Определить размер списка.
    */
   int listSize(List<Integer> values) {
-    throw new TaskNotImplementedException();
+    return values.size();
   }
 
   /**
    * Создайть ArrayList и заполнить его значениями из массива values.
    */
   List<Integer> intList(int... values) {
-    throw new TaskNotImplementedException();
+    List<Integer>list=new ArrayList<>();
+    for (int i=0; i< values.length; i++){
+      list.add(values[i]);
+    }
+    return list;
   }
+
 
   /**
    * Получить элемент c индексом index из списка строк.
    */
   String elementAtIndex(List<String> strings, int index) {
-    throw new TaskNotImplementedException();
+    String a=strings.get(index);
+    return a;
   }
 
   /**
    * Добавить значение value в позицию index в списке values.
    */
   void addAtIndex(List<Long> values, int index, long value) {
-    throw new TaskNotImplementedException();
+    values.add(index,value);
   }
 
   /**
    * Заменить в списке values значение в позиции index новым значением value.
    */
   void updateAtIndex(List<Long> values, int index, long value) {
-    throw new TaskNotImplementedException();
+    values.remove(index);
+    values.add(index,value);
   }
 
   /**
    * Удалить из списка values значение в позиции index.
    */
   void removeAtIndex(List<Long> values, int index) {
-    throw new TaskNotImplementedException();
+    values.remove(index);
   }
 
   /**
    * Удалить из списка values значение value.
    */
   void removeValue(List<Long> values, long value) {
-    throw new TaskNotImplementedException();
+    values.remove(value);
   }
 
   /**
    * Проверить есть ли в списке строк strings строка str.
    */
   boolean hasElement(List<String> strings, String str) {
-    throw new TaskNotImplementedException();
+   if (strings.contains(str)){
+     return true;
+   } else return false;
   }
 
   /**
    * Получить индекс элемента str в списке строк strings.
    */
   int indexOf(List<String> strings, String str) {
-    throw new TaskNotImplementedException();
+    return strings.indexOf(str);
   }
 
   /**
    * Удалить из списка все отрицательные значения и все элементы равные null.
    */
   void removeAllNullOrNegativeValues(List<Integer> values) {
-    throw new TaskNotImplementedException();
+    for (int i=0; i< values.size(); i++){
+    if(values.get(i)==null|| values.get(i)<0){
+       values.remove(i);
+      i--;
+      }
+    }
   }
 
   /**
    * Объединить три списка values1, values2 и values3 в один список.
    */
   List<Integer> mergeLists(List<Integer> values1, List<Integer> values2, List<Integer> values3) {
-    throw new TaskNotImplementedException();
+    List<Integer>list= new ArrayList<>();
+    list.addAll(values1);
+    list.addAll(values2);
+    list.addAll(values3);
+    return list;
   }
 
   /**
    * Определить пустой ли список.
    */
   boolean isEmpty(List<String> strings) {
-    throw new TaskNotImplementedException();
+    if (strings.size()==0){
+      return true;
+    } else return false;
   }
 }
--- src/by/stormnet/task/collections/SetTask.java
+++ src/by/stormnet/task/collections/SetTask.java
@@ -1,8 +1,12 @@
 package by.stormnet.task.collections;
 
 import by.stormnet.task.TaskNotImplementedException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 class SetTask {
 
@@ -10,76 +14,98 @@ class SetTask {
    * Создать пустой HashSet, в котором можно хранить int значения.
    */
   Set<Integer> emptyIntHashSet() {
-    throw new TaskNotImplementedException();
+    Set<Integer>set=new HashSet<>();
+    return set;
   }
 
   /**
    * Создать пустой HashSet, в котором можно хранить String значения.
    */
   Set<String> emptyStringHashSet() {
-    throw new TaskNotImplementedException();
+    Set<String>set=new HashSet<>();
+    return set;
   }
 
   /**
    * Определить размер множества.
    */
   int size(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+    return values.size();
   }
 
   /**
    * Создайть HashSet и заполнить его значениями из массива values.
    */
   Set<Integer> intSetFromArray(int... values) {
-    throw new TaskNotImplementedException();
+    Set<Integer> set= new HashSet<>();
+    for (int i:values) {
+    set.add(i);
+    }
+    return set;
   }
 
   /**
    * Преобразовать список values в HashSet.
    */
   Set<Integer> intSetFromArrayList(List<Integer> values) {
-    throw new TaskNotImplementedException();
+    Set<Integer> set= new HashSet<>();
+    set.addAll(values);
+    return set;
   }
 
   /**
    * Преобразовать множество values в ArrayList.
    */
   List<Integer> intSetToArrayList(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+    List<Integer> list= new ArrayList<>();
+    list.addAll(values);
+    return list;
   }
 
   /**
    * Удалить из множества values значение value.
    */
   void removeValue(Set<Integer> values, int value) {
-    throw new TaskNotImplementedException();
+    values.remove(value);
   }
 
   /**
    * Проверить есть ли во множестве values значение value.
    */
   boolean hasElement(Set<Integer> values, int value) {
-    throw new TaskNotImplementedException();
+    if (values.contains(value)) {
+      return true;
+    } else {
+      return false;
+    }
   }
+
 
   /**
    * Объединить три множества set1, set2, set3 в одно множество.
    */
   Set<Integer> mergeSets(Set<Integer> set1, Set<Integer> set2, Set<Integer> set3) {
-    throw new TaskNotImplementedException();
+    Set<Integer> set= new HashSet<>();
+    set.addAll(set1);
+    set.addAll(set2);
+    set.addAll(set3);
+    return set;
   }
 
   /**
    * Определить пустое ли множество.
    */
   boolean isEmpty(Set<Integer> values) {
-    throw new TaskNotImplementedException();
+    if (values.size()==0){
+      return true;
+    } else return false;
   }
 
   /**
    * Проверить равны ли два множества между собой.
    */
   boolean isEquals(Set<Integer> set1, Set<Integer> set2) {
-    throw new TaskNotImplementedException();
+    boolean a = set1.equals(set2);
+    return a;
   }
 }
